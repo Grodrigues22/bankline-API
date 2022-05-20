da308835df9f60691087faa36352aa27003f9d42
Gabriel Moreira Rodrigues <94092572+Grodrigues22@users.noreply.github.com>
2022-05-20 16:44:42 -0300

    versao-final-do-projeto

--- src/main/java/com/dio/santander/banklineapi/repository/CorrentistaRepository.java
+++ src/main/java/com/dio/santander/banklineapi/repository/CorrentistaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.dio.santander.banklineapi.model.Correntista;
 
 public interface CorrentistaRepository extends JpaRepository<Correntista, Integer>{
+	
+	
 
 }
